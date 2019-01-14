d1c029586c797ea446ad8d1e7cea932edc1f467b
ahmed4545 <34707771+ahmed4545@users.noreply.github.com>
2019-01-14 14:05:18 +0200

    Update trevor1.js

--- trevor1.js
+++ trevor1.js
@@ -9,34 +9,34 @@ client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
      
-  if (message.content.startsWith(adminprefix + 'g')) {
+  if (message.content.startsWith(PrEfix + 'g')) {
     client.user.setGame(argresult);
       message.channel.send(`**✅   ${argresult}**`)
   } else
-     if (message.content === (adminprefix + "leave")) {
+     if (message.content === (PrEfix + "leave")) {
     message.guild.leave();   
   } else  
-  if (message.content.startsWith(adminprefix + 'w')) {
+  if (message.content.startsWith(PrEfix + 'w')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.send(`**✅   ${argresult}**`)
   } else
-  if (message.content.startsWith(adminprefix + 'l')) {
+  if (message.content.startsWith(PrEfix + 'l')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**✅   ${argresult}**`)
   } else
-  if (message.content.startsWith(adminprefix + 's')) {
+  if (message.content.startsWith(PrEfix + 's')) {
     client.user.setGame(argresult, "https://www.twitch.tv/idk");
       message.channel.send(`**✅**`)
   }
-  if (message.content.startsWith(adminprefix + 'name')) {
+  if (message.content.startsWith(PrEfix  + 'name')) {
   client.user.setUsername(argresult).then
       message.channel.send(`Changing The Name To ..**${argresult}** `)
 } else
-  if (message.content.startsWith(adminprefix + 'setprefix')) {
+  if (message.content.startsWith(PrEfix + 'setprefix')) {
   client.user.setPrefix(argresult).then
       message.channel.send(`Changing Prefix ..**${argresult}** `)
 } else
-if (message.content.startsWith(adminprefix + 'avatar')) {
+if (message.content.startsWith(PrEfix + 'avatar')) {
   client.user.setAvatar(argresult);
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
