15b965093f2c0a23ace02f44fcb7fafe9b8febf2
ahmed4545 <34707771+ahmed4545@users.noreply.github.com>
2019-01-14 13:54:00 +0200

    Update trevor1.js

--- trevor1.js
+++ trevor1.js
@@ -8,7 +8,10 @@ const developers = ["533687343480307733"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
-   } else  
+    } else
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();   
+  } else  
   if (message.content.startsWith(adminprefix + 'w')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.send(`**✅   ${argresult}**`)
