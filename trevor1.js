d580802a47dfccb4c984979ffc56946c69290886
ahmed4545 <34707771+ahmed4545@users.noreply.github.com>
2019-01-14 18:18:55 +0200

    Update trevor1.js

--- trevor1.js
+++ trevor1.js
@@ -3,46 +3,27 @@ const client = new Discord.Client();
 
 console.log("Welcome");
 
-const developers = ["533687343480307733"]
 var PrEfix = "nn";
 client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-     
-  if (message.content.startsWith(PrEfix + 'g')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
-  } else
-     if (message.content === (PrEfix + "leave")) {
-    message.guild.leave();   
-  } else  
- if (message.content.startsWith(PrEfix + 'p')) {
-  client.user.setActivity(argresult, {type:'PLAYING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else
-  if (message.content.startsWith(PrEfix + 'w')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else
-  if (message.content.startsWith(PrEfix + 'l')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else
-  if (message.content.startsWith(PrEfix + 's')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/idk");
-      message.channel.send(`**✅**`)
-  }
-  if (message.content.startsWith(PrEfix  + 'name')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-  if (message.content.startsWith(PrEfix + 'setprefix')) {
-  client.user.setPrefix(argresult).then
-      message.channel.send(`Changing Prefix ..**${argresult}** `)
-} else
-if (message.content.startsWith(PrEfix + 'avatar')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+  if (!message.content.startsWith(PrEfix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== '533687343480307733') return;
+if (message.content.startsWith(PrEfix + 'w')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`اهه`)
+} else 
+if (message.content.startsWith(PrEfix + 'p')) {
+client.user.setActivity(argresult, {type:'PLAYING'});
+    message.channel.sendMessage(`اهه`)
+} else 
+if (message.content.startsWith(PrEfix + 'l')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(`اهه`)
+} else 
+if (message.content.startsWith(PrEfix + 's')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`اهه`)
 }
 
 });
